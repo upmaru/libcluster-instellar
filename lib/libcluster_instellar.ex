9b8560f0a2bfae0f8ce35d343c546ae5b3cf604f
Zack Siri <zack@artellectual.com>
2021-03-17 19:45:39 +0700

    initial commit

---
+++ lib/libcluster_instellar.ex
@@ -0,0 +1,18 @@
+defmodule LibclusterInstellar do
+  @moduledoc """
+  Documentation for `LibclusterInstellar`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LibclusterInstellar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/libcluster_instellar_test.exs
@@ -0,0 +1,8 @@
+defmodule LibclusterInstellarTest do
+  use ExUnit.Case
+  doctest LibclusterInstellar
+
+  test "greets the world" do
+    assert LibclusterInstellar.hello() == :world
+  end
+end
